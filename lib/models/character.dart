d094b658f9f7c70851c496a4c0025aba9295209b
Walid Hamdi <walidhamdidev@gmail.com>
2024-11-13 17:54:22 +0100

    Enhanced enum

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,14 +1,16 @@
 import 'package:flutter_rpg/models/stats.dart';
+import 'package:flutter_rpg/models/vocation.dart';
 
 class Character with Stats {
+  Character(this.id, this.name, this._vocation, this.slogan);
+
   final String id;
   final String name;
   final String slogan;
   bool _isFav = false;
+  final Vocation _vocation;
 
   bool get isFav => _isFav;
-
-  Character(this.id, this.name, this.slogan);
 
   void toggleFav() {
     _isFav = !_isFav;
